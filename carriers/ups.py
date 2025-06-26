3b0f40ca6a2c8e0a42615e8e1e9711c010cf01a0
spencerwrong <spencerwong24@gmail.com>
2025-06-26 17:07:49 -0400

    refactor

---
+++ carriers/ups.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from dateutil.parser import isoparse
+import requests
+import time
+from tokens.ups_token import UPSToken
+
+class UPSClient:
+    def __init__(self, client_id: str, client_secret: str, use_sandbox: bool = True):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.token = None
+        self.base_url = "https://wwwcie.ups.com" if use_sandbox else "https://onlinetools.ups.com"
+
+    def ups_generate_new_token(self):
+        url = f"{self.base_url}/security/v1/oauth/token"
+        payload = {
+            "grant_type": "client_credentials"
+        }
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "x-merchant-id": "string"
+        }
+        response = requests.post(url, data=payload, headers=headers, auth=(self.client_id, self.client_secret))
+        response.raise_for_status()
+        self.token = UPSToken(**response.json())
+        return self.token
+
+    def ups_get_token(self):
+        current_time = time.time()
+
+        if self.token:
+            try:
+                issued_at = isoparse(self.token.issued_at).timestamp()
+                expires_in = int(self.token.expires_in)
+                if current_time < issued_at + expires_in:
+                    print(f"[Log] DEBUG: Using cached token (expires at {issued_at + expires_in})")
+                    return self.token
+            except Exception as e:
+                print(f"[Log] DEBUG: Token check failed, regenerating token. Reason: {e}")
+
+        return self.ups_generate_new_token()
+
+    def ups_track(self, inquiry_number: str):
+        access_token = self.ups_get_token().access_token
+        url = f"{self.base_url}/api/track/v1/details/" + inquiry_number
+
+        query = {
+            "locale": "en_US",
+            "returnSignature": "false"
+        }
+
+        headers = {
+            "Content-Type": "application/json",
+            "transId": "string",
+            "transactionSrc": "testing",
+            "Authorization": f"Bearer {access_token}"
+        }
+
+        response = requests.get(url, headers=headers, params=query)
+
+        tracking_data = response.json()
+        return tracking_data
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import configparser
 import datetime
 import discord 
-from ups import ups_track, ups_create_token
+from carriers.ups import UPSClient
 
 from discord.ext import commands
 
@@ -9,13 +9,16 @@ config = configparser.RawConfigParser()
 configFilePath = 'config.ini'
 config.read(configFilePath)
 
-DISCORD_BOT_TOKEN = config.get('Keys', 'discordbottoken')
-UPS_CLIENT_ID = config.get('Keys', 'upsclientid')
-UPS_CLIENT_SECRET = config.get('Keys', 'upsclientsecret')
+DISCORD_BOT_TOKEN = config.get('Discord', 'bot_token')
+UPS_CLIENT_ID = config.get('UPS', 'client_id')
+UPS_CLIENT_SECRET = config.get('UPS', 'client_secret')
+UPS_USE_SANDBOX = config.getboolean('UPS', 'use_sandbox')
 
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+ups_client = UPSClient(UPS_CLIENT_ID, UPS_CLIENT_SECRET, UPS_USE_SANDBOX)
 
 @bot.event
 async def on_ready():
@@ -23,8 +26,7 @@ async def on_ready():
 
 @bot.command()
 async def ups(ctx, tracking_number):
-    access_token = ups_create_token(UPS_CLIENT_ID, UPS_CLIENT_SECRET)['access_token']
-    response = ups_track(tracking_number, access_token)
+    response = ups_client.ups_track(tracking_number)
     
     shipment = response['trackResponse']['shipment'][0]
     package = shipment['package'][0]
@@ -33,7 +35,7 @@ async def ups(ctx, tracking_number):
     status = package['activity'][0]['status']['description']
     formatted_response = f'Tracking Number: {tracking_number}\nStatus: {status}'
 
-    if status == 'DELIVERED ':
+    if status == 'DELIVERED':
         delivery_date = package['deliveryDate'][0]['date']
         delivery_time = package['deliveryTime']['endTime']
         formatted_delivery_date = datetime.datetime.strptime(delivery_date, "%Y%m%d").strftime("%Y/%m/%d")
